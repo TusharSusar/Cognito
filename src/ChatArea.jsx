dfa152da3a0ecccb9acbe4d251227d6162611b28
TusharSusar <tusharsusar06@gmail.com>
2025-09-21 09:52:12 +0530

    Parent Component for Chats

--- src/ChatArea.jsx
+++ src/ChatArea.jsx
@@ -3,12 +3,14 @@ import Sidebar from "./components/Sidebar";
 import InputBar from "./components/InputBar";
 import Chatbox from "./components/Chatbox";
 import Tip from "./components/Tip";
+import Navbar from "./components/Navbar";
 
-const App = () => {
+const ChatArea = () => {
   return (
     <main className="w-full h-dvh max-h-dvh flex bg-bacground">
       <Sidebar />
       <section className="flex-1 flex flex-col p-3">
+        <Navbar/>
         <Chatbox/>
         <InputBar />
         <Tip/>
@@ -17,4 +19,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default ChatArea;

ae849cbd6b8ff537cc98be5fe5cf3f52a8c2cdcf
TusharSusar <tusharsusar06@gmail.com>
2025-10-01 10:29:46 +0530

    Router Updated according to Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home'
-import ChatArea from './ChatArea'
-import ChatContainer from './components/ChatContainer'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import ChatArea from "./ChatArea";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import { ProtectedRoute, PublicRoute } from "./auth/auth";
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<Home/>
+    path: "/",
+    element: <Home />,
   },
   {
-    path:'/chat',
-    element:<ChatArea/>
+    element: <ProtectedRoute />,
+    children: [
+      { path: "/chat", element: <ChatArea /> },
+      { path: "/chat/:cid", element: <ChatArea /> },
+    ],
   },
   {
-    path:'/chat/:cid',
-    element:<ChatArea/>
-  }
-])
+    element: <PublicRoute />,
+    children: [
+      { path: "/signin", element: <Signin /> },
+      { path: "/signup", element: <Signup /> },
+    ],
+  },
+]);
+// {
+//   path:'/chat',
+//   element:<ChatArea/>
+// },
+// {
+//   path:'/chat/:cid',
+//   element:<ChatArea/>
+// },
+// {
+//   path:'/signin',
+//   element:<Signin/>
+// },
+// {
+//   path:'/signup',
+//   element:<Signup/>
+// },
+// ])
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ChatProvider } from "./context/context.jsx";
+import { AuthProvider, ChatProvider } from "./context/context.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
+  <AuthProvider>
     <ChatProvider>
       <App />
     </ChatProvider>
+  </AuthProvider>
   // </StrictMode>
 );

32f1ba0543408a49b87fa574e99d4b0b50e3c40a
TusharSusar <tusharsusar06@gmail.com>
2025-12-29 01:15:53 +0530

    Tip visibility change

--- src/components/Tip.jsx
+++ src/components/Tip.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Tip() {
   return (
-    <div className='w-full mt-2 text-[10px] p-1 text-text text-center'>
+    <div className='hidden sm:visible w-full mt-2 text-[10px] p-1 text-text text-center'>
       Cognito can Mistake <a href='#' className='underline text-primary'>Terms & Codition</a>
     </div>
   )
